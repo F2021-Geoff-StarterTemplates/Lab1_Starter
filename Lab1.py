63c0f59c8e4f5c36b2fe3b4844b590cadc69fbd6
GeoffGillespie <geoff.gillespie@nscc.ca>
2021-09-13 21:16:10 -0300

    Lab initial creation

---
+++ Lab1.py
@@ -0,0 +1,24 @@
+# LAB 1 - BASICS
+# Exploring the print command
+
+# 1. Add a command to display the word Hello.
+
+
+
+# 2. Add a command to display the phrase Oh Canada, our home and native land.
+
+
+
+# 3. Add command(s) to display the lyrics to row, row, row your boat on 4 separate lines.
+
+
+
+# 4. Add a command to display the results of 3 + 4 (ie. result of a math operation)
+
+
+
+# 5. Add commands to save the value "fish" to a variable, then to display the 
+# contents of that variable on screen.
+
+
+
